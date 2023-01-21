2e0a257265d09745a4a5d456b89c575e8a130942
Marmyte <69721776+Marmyte@users.noreply.github.com>
2023-01-21 02:11:29 +0100

    Add files via upload

---
+++ ConfigUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using Dalamud.Interface;
+using ImGuiNET;
+
+namespace NoClippy
+{
+    public static class ConfigUI
+    {
+        public static bool isVisible = false;
+        public static void ToggleVisible() => isVisible ^= true;
+
+        public static void Draw()
+        {
+            if (!isVisible) return;
+
+            //ImGui.SetNextWindowSizeConstraints(new Vector2(400, 200) * ImGuiHelpers.GlobalScale, new Vector2(10000));
+            ImGui.SetNextWindowSize(new Vector2(400, 0) * ImGuiHelpers.GlobalScale);
+            ImGui.Begin("NoClippy Configuration", ref isVisible, ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse);
+            Modules.Modules.Draw();
+            ImGui.End();
+        }
+    }
+}
